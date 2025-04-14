433b74e7e704a4e6dc30da5eb3a2e7038e001c10
Duc Loc <nguyenducloc.dalat@gmail.com>
2025-04-14 14:57:17 +0700

    merge code process

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -6,29 +6,33 @@ import Features from "../../components/Features/Features";
 import Team from "../../components/Team/Team";
 
 const AboutUs = () => {
-  return (
-    <div className="about-container">
-      <div className="about-header">
-        <div className="about-header-content">
-          <h1>About Us</h1>
-          <h2 className="tagline">Where Passion Meets the Plate</h2>
-          <p className="intro-text">
-            Chào mừng đến với Ratatouille – nơi mỗi món ăn là một câu chuyện,
-            mỗi công thức đều chứa đựng truyền thống. Chúng tôi không chỉ đơn
-            thuần chia sẻ công thức nấu ăn – chúng tôi là một cộng đồng những
-            người yêu ẩm thực, cùng nhau mang niềm vui của việc nấu ăn đậm chất
-            truyền thống đến gian bếp của bạn.
-          </p>
+    return (
+        <div className="about-container">
+            <div className="about-header">
+                <div className="about-header-content">
+                    <h1>About Us</h1>
+                    <h2 className="tagline">
+                        Where Passion Meets the Plate
+                    </h2>
+                    <p className="intro-text">
+                        Chào mừng đến với Ratatouille – nơi mỗi món ăn
+                        là một câu chuyện, mỗi công thức đều chứa đựng
+                        truyền thống. Chúng tôi không chỉ đơn thuần
+                        chia sẻ công thức nấu ăn – chúng tôi là một
+                        cộng đồng những người yêu ẩm thực, cùng nhau
+                        mang niềm vui của việc nấu ăn đậm chất truyền
+                        thống đến gian bếp của bạn.
+                    </p>
+                </div>
+            </div>
+            <div className="about-content">
+                <Project />
+                <Features />
+                <VisionMission />
+                <Team />
+            </div>
         </div>
-      </div>
-      <div className="about-content">
-        <Project />
-        <Features />
-        <VisionMission />
-        <Team />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default AboutUs;
