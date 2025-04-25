98f7970b318c5f17d25706053ce471df606026e6
Duc Loc <nguyenducloc.dalat@gmail.com>
2025-04-25 08:31:12 +0700

    resolving merging

--- src/components/AdminSideBoard/AdminSideBoard.js
+++ src/components/AdminSideBoard/AdminSideBoard.js
@@ -6,81 +6,12 @@ import "./AdminSideBoard.css";
 import Logo from "../../assets/img/ratatouille-original.png";
 
 function AdminSideBoard() {
-  const handleLogout = () => {
-    localStorage.removeItem("user");
-    // Clear user data and reload the app to return to the homepage
-    window.location.href = "/";
-  };
+    const handleLogout = () => {
+        localStorage.removeItem("user");
+        // Clear user data and reload the app to return to the homepage
+        window.location.href = "/";
+    };
 
-<<<<<<< HEAD
-  return (
-    <div className="admin-side-board-wrapper">
-      <div className="navigation">
-        <ul>
-          <li>
-            <NavLink className="logo" to="#">
-              <img src={Logo} alt="" />
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink className="side-bar-link" to="#">
-              <span className="icon">
-                <i className="bx bx-home"></i>
-              </span>
-              <span className="title">Tổng quan</span>
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink className="side-bar-link" to="#">
-              <span className="icon">
-                <i className="bx bx-detail"></i>
-              </span>
-              <span className="title">Công thức</span>
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink className="side-bar-link" to="#">
-              <span className="icon">
-                <i class="bx bx-book-alt"></i>
-              </span>
-              <span className="title">Danh mục</span>
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink className="side-bar-link" to="#">
-              <span className="icon">
-                <i className="bx bx-collection"></i>
-              </span>
-              <span className="title">Bộ sưu tập</span>
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink className="side-bar-link" to="#">
-              <span className="icon">
-                <i className="bx bx-user-circle"></i>
-              </span>
-              <span className="title">Người dùng</span>
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink className="side-bar-link" to="#" onClick={handleLogout}>
-              <span className="icon">
-                <i className="bx bx-exit"></i>
-              </span>
-              <span className="title">Đăng xuất</span>
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-=======
     return (
         <div className="admin-side-board-wrapper">
             <div className="navigation">
@@ -153,7 +84,6 @@ function AdminSideBoard() {
             </div>
         </div>
     );
->>>>>>> refs/remotes/origin/main
 }
 
 export default AdminSideBoard;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,98 +4,6 @@ import { Link, NavLink } from "react-router-dom";
 import Logo from "../../assets/img/ratatouille-original.png";
 
 function Footer() {
-<<<<<<< HEAD
-  const { menu, setMenu } = useContext(MenuContext);
-
-  return (
-    <footer className="footer" id="footer">
-      <div className="container">
-        <div className="row">
-          <div className="footer-col">
-            <h4>Ratatouille</h4>
-            <ul>
-              <li>
-                <Link
-                  onClick={() => setMenu("aboutUs")}
-                  className="footer-nav-link"
-                  to="/aboutUs"
-                >
-                  Về chúng tôi
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={() => setMenu("blog")}
-                  className="footer-nav-link"
-                  to="/blog"
-                >
-                  Blog
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={() => setMenu("recipes")}
-                  className="footer-nav-link"
-                  to="/recipes"
-                >
-                  Mục lục công thức
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={() => setMenu("contact")}
-                  className="footer-nav-link"
-                  to="/contact"
-                >
-                  Liên hệ
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          <div className="footer-col">
-            <h4>Món ăn & Công thức</h4>
-            <ul>
-              <li>
-                <Link className="footer-nav-link" to="#">
-                  Nhanh & Dễ dàng
-                </Link>
-              </li>
-              <li>
-                <Link className="footer-nav-link" to="#">
-                  Thuần chay
-                </Link>
-              </li>
-              <li>
-                <Link className="footer-nav-link" to="#">
-                  Nấu súp
-                </Link>
-              </li>
-              <li>
-                <Link className="footer-nav-link" to="#">
-                  Đồ uống
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          <div className="footer-col">
-            <h4>Theo dõi chúng tôi</h4>
-            <div className="social-icon">
-              <a
-                href="https://www.facebook.com/profile.php?id=61574059620067"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="bx bxl-facebook"></i>
-              </a>
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <i className="bx bxl-tiktok"></i>
-              </a>
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <i className="bx bxl-youtube"></i>
-              </a>
-=======
     return (
         <footer className="footer" id="footer">
             <div className="container">
@@ -163,10 +71,10 @@ function Footer() {
                             >
                                 <i className="bx bxl-facebook-circle"></i>
                             </a>
-                            <a href="#" target="_blank" rel="noopener noreferrer">
+                            <a href="#" rel="noopener noreferrer">
                                 <i className="bx bxl-tiktok"></i>
                             </a>
-                            <a href="#" target="_blank" rel="noopener noreferrer">
+                            <a href="#" rel="noopener noreferrer">
                                 <i className="bx bxl-youtube"></i>
                             </a>
                         </div>
@@ -175,25 +83,8 @@ function Footer() {
                         </NavLink>
                     </div>
                 </div>
->>>>>>> refs/remotes/origin/main
             </div>
-            <Link onClick={() => setMenu("home")} to="/">
-              <img src={Logo} alt="Logo" />
-            </Link>
-          </div>
-        </div>
-      </div>
 
-<<<<<<< HEAD
-      <div className="footer-bottom">
-        <p>
-          Copyright &copy;2025; Designed by{" "}
-          <span className="designer">Remy</span>
-        </p>
-      </div>
-    </footer>
-  );
-=======
             <div className="footer-bottom">
                 <p>
                     Copyright &copy;2025; Designed by <span className="designer">Remy</span>
@@ -201,7 +92,6 @@ function Footer() {
             </div>
         </footer>
     );
->>>>>>> refs/remotes/origin/main
 }
 
 export default Footer;
